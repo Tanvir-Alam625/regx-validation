b4e79b1ff746a27bf8b9f99cbf4cc2a53c0de77c
tanveer <mdtanviralamht731@gmail.com>
2022-03-09 15:10:21 +0600

    error message working

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
+const input = document.getElementById('input-field');
 const emailValid = ()=>{
-    const input = document.getElementById('input-field');
     const inputValue = input.value.toLowerCase();
     const  pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     const  pattern1 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
     if(!pattern1.test(inputValue)){
-        console.log('your are not sure');
-        setTimeout(showMessage(true), 300)
+        setTimeout(showMessage(false), 300)
+        setTimeout(removeMessage, 5000)
+
         // showErrorMessage();
     }else{
-        setTimeout(removeMessage, 300)
+        setTimeout(showMessage(true), 300)
+        setTimeout(removeMessage, 5000)
         // removeErrorMessage();
+        input.value = '';
     }
-    input.value = '';
 
 }
 // error message show up 
@@ -24,8 +26,10 @@ const showMessage=(isValid)=>{
     errorId.style.width = '200px'
     if(isValid){
         message.innerText = 'Your Email is Valid';
+        input.style.border = '1px solid green';
         errorId.style.background = 'green';
     }else{
+        input.style.border = '1px solid red';
         message.innerText = 'Your Email is not Valid';
         errorId.style.background = 'red';
     }
