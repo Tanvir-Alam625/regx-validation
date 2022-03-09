a730ea6102d8796d33c40e581f08f11bc2e430e8
tanveer <mdtanviralamht731@gmail.com>
2022-03-09 15:21:40 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
 const input = document.getElementById('input-field');
+// validation function 
 const emailValid = ()=>{
     const inputValue = input.value.toLowerCase();
     const  pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     const  pattern1 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-
     if(!pattern1.test(inputValue)){
         setTimeout(showMessage(false), 300)
         setTimeout(removeMessage, 5000)
-
-        // showErrorMessage();
     }else{
         setTimeout(showMessage(true), 300)
         setTimeout(removeMessage, 5000)
-        // removeErrorMessage();
         input.value = '';
     }
-
 }
-// error message show up 
+// message show up 
 const message = document.getElementById('message');
 const errorId = document.getElementById('error');
+const addMail = document.getElementById('mail');
+// show message function 
 const showMessage=(isValid)=>{
     errorId.style.visibility = 'visible';
-    errorId.style.width = '200px'
+    errorId.style.width = '200px';
     if(isValid){
+        addMail.innerText = '';
         message.innerText = 'Your Email is Valid';
         input.style.border = '1px solid green';
         errorId.style.background = 'green';
+        addMail.innerText = input.value;
     }else{
+        addMail.innerText = '';
         input.style.border = '1px solid red';
         message.innerText = 'Your Email is not Valid';
         errorId.style.background = 'red';
     }
 }
+// remove message function
 const removeMessage=()=>{
     message.innerText = '';
     errorId.style.width = '0px'
     errorId.style.visibility = 'hidden';
-
 }
